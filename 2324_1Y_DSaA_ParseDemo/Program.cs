d699c9fb2cf0472186da7e56cbf5ea842712d267
lenidar <55443074+lenidar@users.noreply.github.com>
2024-02-15 12:02:13 +0800

    Add project files.

---
+++ 2324_1Y_DSaA_ParseDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2324_1Y_DSaA_ParseDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // <dataType> <variableName> = <initialValue>;
+            bool a = true;
+            byte b = 12;
+            short c = 12;
+            int d = 12;
+            long e = 12;
+            float f = 12f;
+            double g = 12d;
+            char h = 'A';
+            string i = "A";
+        }
+    }
+}
